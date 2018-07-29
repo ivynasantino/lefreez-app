4b9ed39d9884ad2ae7ceb8018ff7cc8835ba8a95
alicesilva <alice.silva@ccc.ufcg.edu.br>
2018-07-29 14:33:19 -0300

    video e imagem funcionando

--- lefreez/src/app/app.module.ts
+++ lefreez/src/app/app.module.ts
@@ -3,10 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { VideoPlayer } from '@ionic-native/video-player';
 import { MyApp } from './app.component';
-
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { DesejosPage } from '../pages/desejos/desejos';
@@ -15,6 +13,9 @@ import { LivroPage } from '../pages/livro/livro';
 import { Camera } from '@ionic-native/camera';
 import { StreamingMedia } from '@ionic-native/streaming-media';
 import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { File } from '@ionic-native/file'
+import { PhotoViewer } from '@ionic-native/photo-viewer';
+
 
 @NgModule({
   declarations: [
@@ -45,7 +46,9 @@ import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
     Camera,
     StreamingMedia,
     YoutubeVideoPlayer,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    File,
+    PhotoViewer
   ]
 })
 export class AppModule {}
